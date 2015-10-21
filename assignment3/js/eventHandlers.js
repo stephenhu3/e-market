78337b97f7dc03ce7999059a8603603fcdbd5672
Nathan Yee <ny.nathan.yee@gmail.com>
2015-10-21 12:29:54 -0700

    Fix Assignment 3 task 3

--- assignment3/js/eventHandlers.js
+++ assignment3/js/eventHandlers.js
@@ -4,14 +4,16 @@ $(document).ready(function() {
 	$(".add-cart").click(function() {
 		var productName = $(this).parents(".box").siblings(".product-name").html();
 		addToCart(productName);
-		$(".remove-cart").show();
+		$(this).siblings(".remove-cart").show();
 	});
 
 	$(".remove-cart").click(function() {
 		var productName = $(this).parents(".box").siblings(".product-name").html();
-		removeFromCart(productName);
-		if (jQuery.isEmptyObject(cart)) {
-			$(".remove-cart").hide();
+		if (productName in cart) {
+			removeFromCart(productName);
+		}
+		if (!(productName in cart)) {
+			$(this).hide();
 		}
 	});
 
@@ -19,11 +21,15 @@ $(document).ready(function() {
 		showCart();
 	});
 
-	$(".box").hover(function() {
-		if (jQuery.isEmptyObject(cart)) {
-			$(".remove-cart").hide();
+	$(".thumbnail").hover(function() {
+		var children = $(this).children("h3.product-name");
+		// Ensure that only one product name was found
+		if (children.length === 1 &&
+			 children.html() in cart &&
+			  cart[children.html()] > 0) {
+				$(this).find(".remove-cart").show();
 		} else {
-			$(".remove-cart").show();
+			$(this).find(".remove-cart").hide();
 		}
 	});
 });
