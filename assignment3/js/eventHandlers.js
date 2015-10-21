57d2c9cd63508450dab0176e01cc4f2496962182
Nathan Yee <ny.nathan.yee@gmail.com>
2015-10-20 21:02:03 -0700

    Add assignment3/

---
+++ assignment3/js/eventHandlers.js
@@ -0,0 +1,17 @@
+'use strict';
+
+$(document).ready(function() {
+	$(".add-cart").click(function() {
+		var productName = $(this).parents(".box").siblings(".product-name").html();
+		addToCart(productName);
+	});
+
+	$(".remove-cart").click(function() {
+		var productName = $(this).parents(".box").siblings(".product-name").html();
+		removeFromCart(productName);
+	});
+
+	$(".show-cart").click(function() {
+		showCart();
+	});
+});
