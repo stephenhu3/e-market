301821a1b082f8fd3575612a46021e24250602a5
Nathan Yee <ny.nathan.yee@gmail.com>
2015-10-20 22:13:08 -0700

    Implement Assignment 3 Part 3

--- assignment3/js/eventHandlers.js
+++ assignment3/js/eventHandlers.js
@@ -4,14 +4,26 @@ $(document).ready(function() {
 	$(".add-cart").click(function() {
 		var productName = $(this).parents(".box").siblings(".product-name").html();
 		addToCart(productName);
+		$(".remove-cart").show();
 	});
 
 	$(".remove-cart").click(function() {
 		var productName = $(this).parents(".box").siblings(".product-name").html();
 		removeFromCart(productName);
+		if (jQuery.isEmptyObject(cart)) {
+			$(".remove-cart").hide();
+		}
 	});
 
 	$(".show-cart").click(function() {
 		showCart();
+	});
+
+	$(".box").hover(function() {
+		if (jQuery.isEmptyObject(cart)) {
+			$(".remove-cart").hide();
+		} else {
+			$(".remove-cart").show();
+		}
 	});
 });
